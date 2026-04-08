3275ad442beafe6b5e29cd2229e3343819f87876
PabloSantos-2023251 <psantos-2023251@kinal.edu.gt>
2026-04-08 08:43:14 -0600

    feat: ahora se puede ingresar a la vista de clientes desde la home

--- demo (2)/demo/src/main/java/com/PabloSantos/org/controller/ClienteController.java
+++ demo (2)/demo/src/main/java/com/PabloSantos/org/controller/ClienteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/api/clientes")
 public class ClienteController {
     private final ClienteService clienteService;
     public ClienteController(ClienteService service) { this.clienteService = service; }
---
+++ demo (2)/demo/src/main/java/com/PabloSantos/org/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.PabloSantos.org.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/clientes")
+    public String irAClientes() {
+        return "Cliente";
+    }
+
+}
